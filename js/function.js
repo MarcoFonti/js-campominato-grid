8020b906eca2d1089933bfacdf0fdc3286095cc5
MarcoFonti <theemporiumg@gmail.com>
2024-04-08 17:23:42 +0200

    add commit

--- js/function.js
+++ js/function.js
@@ -9,26 +9,63 @@
 - 4 Stampiamo in pagina
 */
 
-// FUNZIONE DIFICOLTA DIFFICILE
+
+/* CREO UNA FUNZIONE PER LA DIFFICOLTA DIFFICILE E GLI PASSO COME ARGOMENTO LA I */
 const getcellElementsDifficult = (numberDiffucult) => {
+
+    /* CREO UNA VARIBILE E USO IL METODO MANIPOLAZIONE DOM PER CREARE UN DIV */
     const node = document.createElement('div');
+
+
+    /* ASSEGNO ALLA VARIBILE NODE UNA CLASSE HTML */
     node.className = 'cell-difficult';
+
+
+    /* CREO TANTI DIV QUANTO LA I CHE PASSO (10*10) */
     node.innerText = numberDiffucult;
+
+
+    /* RESTITUISCO VARIBILE */
     return node;
 }
 
-// FUNZIONE DIFICOLTA MEDIA
+/* CREO UNA FUNZIONE PER LA DIFFICOLTA MEDIA E GLI PASSO COME ARGOMENTO LA I */
 const getcellElementsMedium = (numberMedium) => {
+
+
+    /* CREO UNA VARIBILE E USO IL METODO MANIPOLAZIONE DOM PER CREARE UN DIV */
     const node = document.createElement('div');
+
+
+    /* ASSEGNO ALLA VARIBILE NODE UNA CLASSE HTML */
     node.className = 'cell-medium';
+
+
+    /* CREO TANTI DIV QUANTO LA I CHE PASSO (9*9) */
     node.innerText = numberMedium;
+
+
+    /* RESTITUISCO VARIBILE */
     return node;
 }
 
-// FUNZIONE DIFICOLTA FACILE
+
+/* CREO UNA FUNZIONE PER LA DIFFICOLTA MEDIA E GLI PASSO COME ARGOMENTO LA I */
 const getcellElementsEasy = (numberEasy) => {
+
+
+    /* CREO UNA VARIBILE E USO IL METODO MANIPOLAZIONE DOM PER CREARE UN DIV */
     const node = document.createElement('div');
+
+
+    /* ASSEGNO ALLA VARIBILE NODE UNA CLASSE HTML */
     node.className = 'cell-easy';
+
+
+    /* CREO TANTI DIV QUANTO LA I CHE PASSO (7*7) */
     node.innerText = numberEasy;
+
+
+    /* RESTITUISCO VARIBILE */
     return node;
 }
--- js/script.js
+++ js/script.js
@@ -12,15 +12,20 @@ console.log('JS OK');
 - 4 Stampiamo in pagina
 */
 
-// RECUPERO ELEMENTI
+
+/* RECUPERO ELEMENTI IN BASE ALL'ID E ALLA CLASSE */
 const formElement = document.getElementById('form');
 const selectField = document.getElementById('recipe-select');
 const buttonField = document.querySelector('.recipe-button');
 const squaresElement = document.querySelector('.recipe-squares');
 
+
+/* CONTROLLO RISPOSTA */
 console.log(formElement, selectField, buttonField, squaresElement);
 
-// VARIABILI CHE CONOSCO
+
+
+/* CREO VARIBILE CHE CONOSCO PER STABILE QUANTE COLLONE CI SARANNO */
 const rowsDifficult = 10
 const colsDifficult = 10
 const totDifficult = rowsDifficult * colsDifficult
@@ -34,67 +39,124 @@ const colsEasy = 7
 const totEasy = rowsEasy * colsEasy
 
 
-
-    // EVENTO AL CLICK
+/* ALL'EVENTO DEL SUBMIT SUL FORM */
 formElement.addEventListener('submit', (e) => {
 
-    
+
+    /* BLOCCO IL CARICAMENTO PREDIFINITO */
     e.preventDefault();
 
+
+    /* RIASSEGNO IL BOTTONE PLAY CON RICOMINCIA */
     buttonField.innerText = 'Ricomincia'
 
+
+    /* ALL'EVENTO SUL BOTTONE RICOMINCIA */
     buttonField.addEventListener('click', ()=>{
+
+        /* SVUOTO LE CELLE SELEZIONATI DALL'UTENTE */
         squaresElement.innerText = ''
     })
 
-    // RECUPERO DATI UTENTE 
+    
+    /* CREO UNA VARIBILE E RECUPERO IL VALUE CHE SELEZIONA NELLA SELECT L'UTENTE */
     const difficulty = selectField.value;
+
+
+    /* CONTROLLO RIPOSTA */
     console.log(difficulty);
     
-    // SE
+    
+    /* SE LA MIA VARIBILE A CUI HO SALAVATO IL VALUE DELL'UTENTE E' IDENTICA A 1 */
     if (difficulty === '1') {
 
+
+        /* CREO CICLO FOR SU LE COLLONE TOTALI (10*10) */
         for (let i = 1; i <= totDifficult; i++) {
         
+
+            /* METTO LA FUNZIONE IN UNA VARIBILE E GLI PASSO LA I (NUMERI DA 1 A 10*10) */
             const cellElementsDiffiucult = getcellElementsDifficult(i);
         
+
+            /* ALL'EVENTO CLICK SU OGNI SINGOLA CELLA(DIV) CREATA DALLA FUNZIONE */
             cellElementsDiffiucult.addEventListener('click', () => {
+
+
+                /* ALLA VARIBILE A CUI PASSO LA FUNZIONE DI CREAZIONE DI OGNI CELLA(DIV), GLI CREO UN TOGGLE IN MODO DA COLORAMELA O NON */
                 cellElementsDiffiucult.classList.toggle('color-cell')
+
+
+                /* CONTROLLLO RISPOSTA */
                 console.log(cellElementsDiffiucult.innerText);
             })
         
+
+            /* AGGIUNGO ALL'EMELENTO SQUERESELEMENTE OGNI SINGOLA CELLA(DIV) CHE CREO (DETTO NODO) */
             squaresElement.appendChild(cellElementsDiffiucult);
         
         }
 
-    // ALTRIMENTI SE 
+    
+        
+        /* ALTRIMENTI SE LA MIA VARIBILE A CUI HO SALAVATO IL VALUE DELL'UTENTE E' IDENTICA A 2 */
     } else if (difficulty === '2') {
 
+
+        /* CREO CICLO FOR SU LE COLLONE TOTALI (9*9) */
         for (let i = 1; i <= totMedium; i++) {
         
+
+            /* METTO LA FUNZIONE IN UNA VARIBILE E GLI PASSO LA I (NUMERI DA 1 A 9*9) */
             const cellElementsMedium = getcellElementsMedium(i);
         
+
+            /* ALL'EVENTO CLICK SU OGNI SINGOLA CELLA(DIV) CREATA DALLA FUNZIONE */
             cellElementsMedium.addEventListener('click', () => {
+
+
+                /* ALLA VARIBILE A CUI PASSO LA FUNZIONE DI CREAZIONE DI OGNI CELLA(DIV), GLI CREO UN TOGGLE IN MODO DA COLORAMELA O NON */
                 cellElementsMedium.classList.toggle('color-cell')
+
+
+                /* CONTROLLLO RISPOSTA */
                 console.log(cellElementsMedium.innerText);
             })
         
+
+            /* AGGIUNGO ALL'EMELENTO SQUERESELEMENTE OGNI SINGOLA CELLA(DIV) CHE CREO (DETTO NODO) */
             squaresElement.appendChild(cellElementsMedium);
         
         }
 
-    // ALTRIMENTI SE 
+
+
+        /* ALTRIMENTI SE LA MIA VARIBILE A CUI HO SALAVATO IL VALUE DELL'UTENTE E' IDENTICA A 3 */
     } else if (difficulty === '3') {
 
+
+        /* CREO CICLO FOR SU LE COLLONE TOTALI (7*7) */
         for (let i = 1; i <= totEasy; i++) {
         
+
+            /* METTO LA FUNZIONE IN UNA VARIBILE E GLI PASSO LA I (NUMERI DA 1 A 7*7) */
             const cellElementsEasy = getcellElementsEasy(i);
-        
+            
+
+            /* ALL'EVENTO CLICK SU OGNI SINGOLA CELLA(DIV) CREATA DALLA FUNZIONE */
             cellElementsEasy.addEventListener('click', () => {
+
+
+                /* ALLA VARIBILE A CUI PASSO LA FUNZIONE DI CREAZIONE DI OGNI CELLA(DIV), GLI CREO UN TOGGLE IN MODO DA COLORAMELA O NON */
                 cellElementsEasy.classList.toggle('color-cell')
+
+
+                /* CONTROLLLO RISPOSTA */
                 console.log(cellElementsEasy.innerText);
             })
         
+
+            /* AGGIUNGO ALL'EMELENTO SQUERESELEMENTE OGNI SINGOLA CELLA(DIV) CHE CREO (DETTO NODO) */
             squaresElement.appendChild(cellElementsEasy);
         
         }
